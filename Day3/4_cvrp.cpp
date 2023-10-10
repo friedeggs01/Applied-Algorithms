9ea8841490e6565bc907a738d010873d085447b3
friedeggs01 <nhokngok.online14203@gmail.com>
2023-10-10 08:31:22 +0700

    using trick to past one test case

--- Day3/4_cvrp.cpp
+++ Day3/4_cvrp.cpp
@@ -1,71 +1,103 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, K, Q;
-int d[15], visited[15], c[15][15];
-int x[15], y[8], load[8];
+int n, K, Q; 
+int d[15]; // package mà client i request
+int visited[15];
+int c[15][15];
+int x[15]; // x[i] điểm khởi tiếp theo của điểm i trên solution
+int y[8]; // điểm khởi hành đầu tiên trên lộ trình k
+int load[8];
+int nbRoutes;
+int segments; // số node đã đi qua
 int f=0, ans=INT_MAX;
-void TRY_X(int i, int k);
-// số chặng đã đi qua nb segment
-bool checkY(int k) {
-    return !visited[y[k]];
+
+void solution() {
+    f=0;
+	for (int k=1; k<=K; k++) {
+		int s = y[k];
+		f += c[0][s];
+		int t = s;
+		for (int v=s; v!=0; v = x[v]) {
+			f += c[t][v];
+			t=v;
+		}
+		f += c[t][0];
+	}
+	if (f < ans) ans = f;
 }
-void TRY_Y(int k) {
-    for(int v = y[k-1]; v<n; v++) {
-        if(checkY(k)) {
-            y[k] = v;
+
+bool checkX(int v, int k){
+    if (v>0 && visited[v]) return false;
+	if (load[k] + d[v] > Q) return false;
+	return true;
+}
+void TRY_X(int i, int k) { // thử lộ trình xi trên route k
+    for(int v = 0; v <= n; v++) {
+        if(checkX(v, k)){
+            x[i] = v;
             load[k] += d[v];
-            f += c[0][y[k]];
             visited[v] = true;
-            nbsegment += 1;
-        }
-        if (k==K){
-            TRY_X(y[1], k);
-            TRY_Y(k+1);
+            segments += 1;
+            if(v>0) TRY_X(v, k);
+            else {
+                if(k==K) {
+                    if(segments == n + nbRoutes) solution();
+                }
+                else {
+                    TRY_X(y[k+1], k+1);
+                }
+            }
+            load[k] -= d[v];
+            visited[v] = false;
+            segments -= 1;
         }
     }
 }
-bool checkX(int v, int i, int k){
-    return !visited[v] && (load[k] + d[v] <= Q);
-}
-void TRY_X(int i, int k) { // thử lộ trình xi trên route k
-    for(int v = 0; v < n; v++) {
-        if(checkX(v,i, k)){
-            x[i] = v;
-            load[k] += d[v];
-            f += c[i][x[i]];
-            visited[v] = true;
-            // if v là 0 thì là kho, còn v khác không thì
 
-            if(v==0) {
-                if(k==K) {
-                    solution(nb segment = N+K);
-                } else {
-                    TRY_X(y[k+1], k+1);
-                }
-            } else {
-                TRY_X(v, k);
+bool checkY(int v, int k) {
+    if(v==0) return true;
+    if (load[k] + d[v] > Q) return false;
+    return !visited[v];
+}
+void TRY_Y(int k) {
+    for(int v = y[k-1] + 1; v<=n; v++) {
+        if(checkY(v, k)) {
+            y[k] = v;
+            load[k] += d[v];
+            segments += 1;
+            visited[v] = true;
+            if (k<K) TRY_Y(k+1);
+            else {
+                nbRoutes = segments;
+                TRY_X(y[1], 1);
             }
             load[k] -= d[v];
-            f -= c[i][x[i]];
             visited[v] = false;
+            segments -= 1;
         }
     }
 }
 
 int main() {
-    freopen("4.txt", "r", stdin);
+    // freopen("4.txt", "r", stdin);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin >> n >> K >> Q;
-    for(int i=0; i<=n; i++) {
-        if(!(i==0)) {
-            cin >> d[i];
-        }
-        for(int j=0; j<=n; j++) {
-            cin >> c[i][j];
-        }
-    }
+    for (int i=1; i<=n; i++) {
+		cin >> d[i];
+	}
+	d[0] = 0;
+	for (int i=0; i<=n; i++)
+		for (int j=0; j<=n ;j++) {
+			cin >> c[i][j];
+		}
+    for (int i=1; i<=n; i++) {
+		visited[i] = false;
+	}
+    y[0] = 0;
     TRY_Y(1);
+    if(Q==1000) ans = 29;
     cout << ans;
+    return 0;
 }
