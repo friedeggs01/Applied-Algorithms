776c5f2d7222356c8705bf93ce5381f058acaf97
friedeggs01 <nhokngok.online14203@gmail.com>
2023-11-10 18:35:17 +0700

    dynamic programming easy

---
+++ Leetcode/1_get_maximum_in_generated_array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if(n==0 || n==1) {
+            return n;
+        }
+        vector<int> nums(101, 0);
+        nums[1] = 1;
+        int maxi = 0;
+        for(int i=2; i<=n; i++) {
+            if(i%2==0) {
+                nums[i] = nums[i/2];
+            } else {
+                nums[i] = nums[i/2] + nums[i/2 + 1];
+            }
+            maxi = max(maxi, nums[i]);
+        }
+        return maxi;
+    }
+};
---
+++ Leetcode/3_climbing_stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1 || n==2) {
+            return n;
+        }
+        vector<int> dp(46, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i=3;i<=n;++i){
+            dp[i]=dp[i-1]+dp[i-2]; // if you reach (i-2) just make 2 step, if you reach (i-1) just make 1 step 
+        }
+        return dp[n];
+    }
+};
