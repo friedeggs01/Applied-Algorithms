83e77e473b7c17ff3944e8f68fe97eeada89f0f2
friedeggs01 <nhokngok.online14203@gmail.com>
2023-11-21 17:20:28 +0700

    search from the center

---
+++ Leetcode/6_longest_palindromic_substring.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+    
+string s;
+
+string longestPalindrome() {
+    if (s.length() <= 1) {
+        return s;
+    }
+    
+    int max_len = 1;
+    int start = 0;
+    int end = 0;
+    vector<vector<bool>> dp(s.length(), vector<bool>(s.length(), false));
+    
+    for (int i = 0; i < s.length(); ++i) {
+        dp[i][i] = true;
+        for (int j = 0; j < i; ++j) {
+            if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
+                dp[j][i] = true;
+                if (i - j + 1 > max_len) {
+                    max_len = i - j + 1;
+                    start = j;
+                    end = i;
+                }
+            }
+        }
+    }
+    
+    return s.substr(start, end - start + 1);
+}
+
+
+int main() {
+    ios_base::sync_with_stdio();
+    cin.tie(); cout.tie();
+    cin >> s;
+    cout << longestPalindrome();
+}
+
+
