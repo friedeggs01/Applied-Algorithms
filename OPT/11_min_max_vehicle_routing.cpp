a34156742187ed30c56c36d608028efce1de5051
friedeggs01 <nhokngok.online14203@gmail.com>
2024-01-21 07:00:40 +0700

    np-hard with n points, k workers

--- OPT/11_min_max_vehicle_routing.cpp
+++ OPT/11_min_max_vehicle_routing.cpp
@@ -1,16 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, k,f[1001][1001], d[1001];
+int n, k,f[1001][1001];
+vector<int> r[1001];
 
 int calculate_fitness(vector<int> path){
     int res=0;
     res += f[0][path[0]];
-    res += d[path[0]];
     // cout << res << endl;
     int i = 0;
     for(i = 0; i<path.size()-1; i++) {
-        res += d[path[i+1]];
         res += f[path[i]][path[i+1]];
         // cout << res << endl;
     }
@@ -35,54 +34,48 @@ vector<int> randomly_swap(vector<int> path){
     return path;
 }
 
-
 int main() {
     ios_base::sync_with_stdio();
     cin.tie(); cout.tie();
-    freopen("5.txt", "r", stdin);
+    freopen("11.txt", "r", stdin);
     cin >> n >> k;
-    int t[n+1][n+1];
-    vector<int> r[n+1];
+    int  d[n+1][n+1];
     bool visited[n+1];
     memset(visited, false, sizeof(visited));
     visited[n] = false;
-    // cout << visited[5];
     visited[0] = true;
-    for(int i=1; i<=n; i++) {
-        cin >> d[i];
-    }
     for(int i=0; i<=n; i++) {
         for(int j=0; j<=n; j++) {
-            cin >> t[i][j];
-            f[i][j] = t[i][j];
-            // cout << t[i][j] << " ";
+            cin >> d[i][j];
+            f[i][j] = d[i][j];
+            // cout << d[i][j] << " ";
         }
         // cout << endl;
-        t[i][i] = INT_MAX;
+        d[i][i] = INT_MAX;
     }
     for(int i=1; i<=k; i++) {
-        int* minElement = min_element(t[0], t[0] + n);
-        int j = minElement - t[0];
+        int* minElement = min_element(d[0], d[0] + n);
+        int j = minElement - d[0];
         if(!visited[j]) {
             visited[j] = true;
             r[i].push_back(j);
-            t[0][j] = INT_MAX;
+            d[0][j] = INT_MAX;
         }
-        // cout << r[i][0] << endl;
+        cout << r[i][0] << endl;
     }
-    while(count(visited, visited + n, false) > 0){
+    // for(int i=0; i<=n; i++) {
+    //     cout << visited[i] << " ";
+    // }
+    while(count(visited, visited + n+1, false) > 0){
         for(int i=1; i<=k; i++) {
             int cnt = 0;
-            while(true) {
-                cnt++;
+            while(true) {    
+                cnt++;        
+                // cout << "visited[5]: " << visited[5]<< endl;
                 // cout << "worker: " << i << " r[i][r[i].size()-1] " << r[i][r[i].size()-1] << endl;
-                // for(auto v : t[r[i][r[i].size()-1]]){
-                //     cout << v << " ";
-                // }
-                // cout << endl;
-                int* minElement = min_element(t[r[i][r[i].size()-1]], t[r[i][r[i].size()-1]] + n+1);
-                int j = minElement - t[r[i][r[i].size()-1]];
-                t[r[i][r[i].size()-1]][j] = INT_MAX;
+                int* minElement = min_element(d[r[i][r[i].size()-1]], d[r[i][r[i].size()-1]] + n+1);
+                int j = minElement - d[r[i][r[i].size()-1]];
+                d[r[i][r[i].size()-1]][j] = INT_MAX;
                 if(j == 0) continue;
                 // cout << j << endl;
                 if(!visited[j]) {
@@ -90,21 +83,24 @@ int main() {
                     r[i].push_back(j);
                     break;
                 }
-                if(cnt == 4) break;
+                if(cnt == 2) break;
             }
         }
+        // for(int i=0; i<=n; i++) {
+        //     cout << visited[i] << " ";
+        // }
     }
     cout << k << endl;
     for(int i=1; i<=k; i++) {
-        cout << r[i].size()+2 << endl;
+        cout << r[i].size()+1 << endl;
         cout << 0 << " ";
         for(auto j : r[i]) {
             cout << j << " ";
         }
-        cout << 0 << endl;
+        cout << endl;
     }
     for(int i=1; i<=k; i++) {
-        cout << "i: " << i << endl;
+        cout << endl << "i: " << i << endl;
         int cnt = 0;
         while(true) {
             cnt++;
@@ -112,20 +108,22 @@ int main() {
             cout << res;
             vector<int> new_path = randomly_swap(r[i]);
             int res2 = calculate_fitness(new_path);
-            if(cnt >= 10 || res2 < res){
+            if(res2 < res){
                 r[i] = new_path;
                 cout << res2 << endl;
+                break;
+            } else if (cnt >= 10){
                 break;
             }
         }
     }
     // cout << k << endl;
     // for(int i=1; i<=k; i++) {
-    //     cout << r[i].size()+2 << endl;
+    //     cout << r[i].size()+1 << endl;
     //     cout << 0 << " ";
     //     for(auto j : r[i]) {
     //         cout << j << " ";
     //     }
-    //     cout << 0 << endl;
+    //     cout << endl;
     // }
 }
---
+++ final_cs/final_2022.2/analyze_delivery_trips.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    ios_base::sync_with_stdio();
+    cin.tie(); cout.tie();
+    freopen("1.txt", "r", stdin);
+}
